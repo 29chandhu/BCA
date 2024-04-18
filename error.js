d64a398041f6d4f302e34d5e3082593e0d94eb82
29Chandhu <chandanabumi2004@gmail.com>
2024-04-18 14:55:39 +0530

    18-04-24

--- error.js
+++ error.js
@@ -1,54 +1,60 @@
-// //throw
-// //throw 42;
-// //throw 'Ball';
+//throw expression;
+
+//throw 42;
+//throw 'Ball';
 // throw {
 //     head: 'Tail'
 // }
-// //try ....catch...finally
-// let divide =(a,b)=> {
-//     try{
-//         if(b==0){
-//             throw "division by 0"
-//         }else{
-//             return a/b;
+
+//try...catch...finally
+
+// let divide = (a, b) => {
+//     try {
+//         if(b == 0) {
+//             throw "Division by zero"
+//         } else {
+//             return a / b;
 //         }
-//     } catch(e){
+//     } catch(e) {
 //         console.log(e);
-//         console.log('why are you dividing by 0?')
+//         console.log('Why are you dividing by 0?')
 //     }
 // }
-// let sum= (a,b,c)=>{
-//     let div = divide(a,b);
-//     return div +c;
+
+// let sum = (a, b, c) => {
+//     let div = divide(a, b);
+//     return div + c;
 // }
-// console.log(sum(1,0,2))
-let area= (length,width) =>{
-    try {      
-        if(length <0 || width <0){
-            throw 'negative values'
+
+// console.log(sum(1, 0, 2))
+// compute area only when length and width are positive numbers
+let area = (length, width) => {
+    try {
+        if(length < 0 || width < 0) {
+            throw 'Negative values'
         } else {
             return length * width;
         }
-    } catch(e){
+    } catch(e) {
         alert(e);
-
     }
 }
-console.log(area(-4,9))
+console.log(area(-4, 9))
 
 //sum array
-let sumArray =(a) => {
-    try{
-     if(a.length==0){
-   } else {
-     let sum=0;
-     for(let i=0;i<a.length;i++) {
-        sum=sum+ a[i];
-     }
-     return sum;
-   }
-  } catch(e){
-     console.log(e);
-  }
+let sumArray = (a) => {
+    try {
+        if(a.length == 0) {
+            throw "Empty array";
+        } else {
+            let sum = 0;
+            for(let i = 0; i < a.length; i++) {
+                sum = sum + a[i];
+            }
+            return sum;
+        }
+    } catch(e) {
+        console.log(e);
+    }
 }
-console.log(sumArray([1,2,3]));
+console.log(sumArray([1, 2, 3]));
